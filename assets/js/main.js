89b20bb885ff2722162c37c5d1c9deb08c369091
cona991 <30394588+cona991@users.noreply.github.com>
2017-09-17 21:04:36 +1200

    project2

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+
+(function($) {
+
+	
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+
+
+			$('.scrolly').scrolly();
+
+			$('.gallery').each(function() {
+
+				var	$gallery = $(this),
+					$content = $gallery.find('.content');
+
+
+				// Poptrox.
+					$content.poptrox({
+						usePopupCaption: true
+					});
+
+					
+
+				// Tabs.
+					$gallery.each( function() {
+
+						//var $this = $(this),
+							//$tabs = $this.find('.tabs a'),
+							$media = $this.find('.media');
+
+						$tabs.on('click', function(e) {
+
+							//var $this = $(this),
+								//tag = $this.data('tag');
+
+							
+
+							// Hide media that do not have the same class as the clicked tab.
+								$media
+									.fadeOut('fast')
+									.each(function() {
+
+										var $this = $(this);
+
+										
+
+									});
+
+						});
+
+					});
+
+
+			});
+
+			$('.userinfo').each(function() {
+
+				var	$userinfo = $(this),
+					$Login = $userinfo.find('.Login');
+
+
+				// Poptrox.
+					$Login.poptrox({
+						usePopupCaption: true
+					});
+
+					
+
+				// Tabs.
+					$userinfo.each( function() {
+
+						//var $this = $(this),
+							//$tabs = $this.find('.tabs a'),
+							$page = $this.find('.page');
+
+						$tabs.on('click', function(e) {
+
+							//var $this = $(this),
+								//tag = $this.data('tag');
+
+							
+
+							// Hide media that do not have the same class as the clicked tab.
+								$page
+									.fadeOut('fast')
+									.each(function() {
+
+										var $this = $(this);
+
+										
+
+									});
+
+						});
+
+					});
+
+
+			});
+
+
+
+	});
+
+})(jQuery);
